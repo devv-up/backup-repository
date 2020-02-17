28dbb17d4fe1f3129ce3bdbc8c965e060179081b
choitaehoon1998 <hugebidream@gmail.com>
2020-02-17 19:57:04 +0900

    reset

--- dev_up_backend/wsgi.py
+++ dev_up_backend/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mysettings project.
+WSGI config for dev_up_backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysettings.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dev_up_backend.settings')
 
 application = get_wsgi_application()
